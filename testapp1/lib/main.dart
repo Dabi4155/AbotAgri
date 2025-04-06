ee74f96f6471d8b34a7d3fa4c4a20ceb12176cc7
Lukecobs <cobrado.lukelouise123@gmail.com>
2025-04-06 13:21:38 +0800

    Update main.dart

--- testapp1/lib/main.dart
+++ testapp1/lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application. 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
@@ -25,3 +25,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// this is archama von dutch del mar. 
